42fc9cd95f46d3f356f8a5ecdcccd69288703e3c
guy pereg <guypereg@campus.technion.ac.il>
2017-05-06 14:11:33 +0300

    sys

--- challenge_system.c
+++ challenge_system.c
@@ -6,6 +6,7 @@
 
 
 #include "challenge_system.h"
+#include "challenge_room_system_fields.h"
 
 #define ROW_LENGTH 51
 
@@ -19,6 +20,46 @@ Result create_system(char *init_file, ChallengeRoomSystem **sys){
         return NULL_PARAMETER;
     }
     fgets(buffer,ROW_LENGTH,file);
+    if (buffer == NULL){
+        return NULL_PARAMETER;
+    }
+    sys->*name=malloc(sizeof(char*)*strlen(buffer));
+    if (*sys->(*name) == NULL){
+        return NULL_PARAMETER;
+    }
+    /* I'm not sure about the Syntax *sys->(*name) */
+    strcpy(*sys->(*name),buffer);
     
     return OK;
 }
+
+
+Result destroy_system(ChallengeRoomSystem *sys, int destroy_time,
+                      char **most_popular_challenge_p, char **challenge_best_time);
+
+
+Result visitor_arrive(ChallengeRoomSystem *sys, char *room_name, char *visitor_name, int visitor_id, Level level, int start_time);
+
+
+Result visitor_quit(ChallengeRoomSystem *sys, int visitor_id, int quit_time);
+
+
+Result all_visitors_quit(ChallengeRoomSystem *sys, int quit_time);
+
+
+Result system_room_of_visitor(ChallengeRoomSystem *sys, char *visitor_name, char **room_name);
+
+
+Result change_challenge_name(ChallengeRoomSystem *sys, int challenge_id, char *new_name);
+
+
+Result change_system_room_name(ChallengeRoomSystem *sys, char *current_name, char *new_name);
+
+
+Result best_time_of_system_challenge(ChallengeRoomSystem *sys, char *challenge_name, int *time);
+
+
+Result most_popular_challenge(ChallengeRoomSystem *sys, char **challenge_name);
+
+
+#endif // CHALLENGE_SYSTEM_H_
